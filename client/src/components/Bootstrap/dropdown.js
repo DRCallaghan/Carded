be9ef08a13add298ffd0b27781501d5bcf43e2f2
Chris Kratz <Ckratz17@gmail.com>
2022-08-31 19:18:37 -0400

    added modal on profile page for team search

--- client/src/components/Bootstrap/dropdown.js
+++ client/src/components/Bootstrap/dropdown.js
@@ -1,22 +1,20 @@
 import React from 'react';
-import Button from 'react-bootstrap/Button';
-import ButtonGroup from 'react-bootstrap/ButtonGroup';
 import Dropdown from 'react-bootstrap/Dropdown';
+import DropdownButton from 'react-bootstrap/DropdownButton';
 
 function Drop() {
     return (
-      <Dropdown as={ButtonGroup}>
-        <Button variant="success">Split Button</Button>
-  
-        <Dropdown.Toggle split variant="success" id="dropdown-split-basic" />
-  
-        <Dropdown.Menu>
-          <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-          <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-          <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
-        </Dropdown.Menu>
-      </Dropdown>
+        <DropdownButton
+            align="end"
+            title="Team Pages"
+            id="dropdown-menu-align-end"
+        >
+            <Dropdown.Item eventKey="1">Add Team</Dropdown.Item>
+            <Dropdown.Divider />
+            <Dropdown.Item eventKey="4">Separated link</Dropdown.Item>
+        </DropdownButton>
     );
-  }
-  
-  export default Drop;
+}
+
+
+export default Drop;
