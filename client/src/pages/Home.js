081727cb883cc14efdd3b1776f61ac15e2b23edc
DRCallaghan <dennis.callaghan87@gmail.com>
2022-09-01 10:19:11 -0400

    home styling and info

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -14,96 +14,97 @@ const Home = () => {
       <Header />
       <div className="flex-row justify-center">
         <div className="col-12 col-md-10 my-3">
-          {/* {loading ? (
-            <div>Loading...</div>
-          ) : (
-            // <ProfileList
-            //   profiles={profiles}
-            //   title="Here's the current roster of friends..."
-            // />
-          )} */}
         </div>
       </div>
-      <div className="summary"><h2>summary section here</h2></div>
-      <div className="examples"><h2>examples sections here</h2></div>
-      <div className="card-example">
+      <div className="flex-row justify-center card-example">
         <img src={carded} alt="card" class="card" />
-        <text> example card </text>
       </div>
+      <div className="flex-row justify-center summary">
+        <h2>Welcome to Carded!</h2>
+      </div>
+      <div className="flex-row justify-center summary">
+        <div className="col-12 col-md-8 text-center my-3">
+          <p>Carded is a virtual business card solution that gives you control over how your business cards look both in-person and online. Carded brings your business cards to LinkedIn and your emails while fitting more information into them than ever before. With the flexibility our team management system brings and the ability to generate new business cards at the touch of a button, you'll always want to get Carded.</p>
+        </div>
+      </div>
+      <div className="flex-row justify-center examples"><h2>examples sections here</h2></div>
 
-      <div className="in-depth">in depth info here</div>
-      <div className="management">
-       <h2>management details here</h2> 
-      
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime,
-          ratione. Qui quod, dolores, veritatis itaque voluptates quaerat odit
-          laborum praesentium, neque provident reiciendis harum recusandae
-          excepturi aliquam repellat laboriosam facere.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime,
-          ratione. Qui quod, dolores, veritatis itaque voluptates quaerat odit
-          laborum praesentium, neque provident reiciendis harum recusandae
-          excepturi aliquam repellat laboriosam facere.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime,
-          ratione. Qui quod, dolores, veritatis itaque voluptates quaerat odit
-          laborum praesentium, neque provident reiciendis harum recusandae
-          excepturi aliquam repellat laboriosam facere.
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime,
-          ratione. Qui quod, dolores, veritatis itaque voluptates quaerat odit
-          laborum praesentium, neque provident reiciendis harum recusandae
-          excepturi aliquam repellat laboriosam facere.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime,
-          ratione. Qui quod, dolores, veritatis itaque voluptates quaerat odit
-          laborum praesentium, neque provident reiciendis harum recusandae
-          excepturi aliquam repellat laboriosam facere.
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime,
-          ratione. Qui quod, dolores, veritatis itaque voluptates quaerat odit
-          laborum praesentium, neque provident reiciendis harum recusandae
-          excepturi aliquam repellat laboriosam facere.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime,
-          ratione. Qui quod, dolores, veritatis itaque voluptates quaerat odit
-          laborum praesentium, neque provident reiciendis harum recusandae
-          excepturi aliquam repellat laboriosam facere.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime,
-          ratione. Qui quod, dolores, veritatis itaque voluptates quaerat odit
-          laborum praesentium, neque provident reiciendis harum recusandae
-          excepturi aliquam repellat laboriosam facere.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime,
-          ratione. Qui quod, dolores, veritatis itaque voluptates quaerat odit
-          laborum praesentium, neque provident reiciendis harum recusandae
-          excepturi aliquam repellat laboriosam facere.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime,
-          ratione. Qui quod, dolores, veritatis itaque voluptates quaerat odit
-          laborum praesentium, neque provident reiciendis harum recusandae
-          excepturi aliquam repellat laboriosam facere.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime,
-          ratione. Qui quod, dolores, veritatis itaque voluptates quaerat odit
-          laborum praesentium, neque provident reiciendis harum recusandae
-          excepturi aliquam repellat laboriosam facere.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime,
-          ratione. Qui quod, dolores, veritatis itaque voluptates quaerat odit
-          laborum praesentium, neque provident reiciendis harum recusandae
-          excepturi aliquam repellat laboriosam facere.
-        </p>
-        
+
+      <div className="flex-row justify-center in-depth">in depth info here</div>
+      <div className="flex-row justify-center management">
+        <h2>management details here</h2>
+        <div className="flex-row justify-center management">
+          <div className="col-12 col-md-8 text-center my-3">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime,
+              ratione. Qui quod, dolores, veritatis itaque voluptates quaerat odit
+              laborum praesentium, neque provident reiciendis harum recusandae
+              excepturi aliquam repellat laboriosam facere.
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime,
+              ratione. Qui quod, dolores, veritatis itaque voluptates quaerat odit
+              laborum praesentium, neque provident reiciendis harum recusandae
+              excepturi aliquam repellat laboriosam facere.
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime,
+              ratione. Qui quod, dolores, veritatis itaque voluptates quaerat odit
+              laborum praesentium, neque provident reiciendis harum recusandae
+              excepturi aliquam repellat laboriosam facere.
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime,
+              ratione. Qui quod, dolores, veritatis itaque voluptates quaerat odit
+              laborum praesentium, neque provident reiciendis harum recusandae
+              excepturi aliquam repellat laboriosam facere.
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime,
+              ratione. Qui quod, dolores, veritatis itaque voluptates quaerat odit
+              laborum praesentium, neque provident reiciendis harum recusandae
+              excepturi aliquam repellat laboriosam facere.
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime,
+              ratione. Qui quod, dolores, veritatis itaque voluptates quaerat odit
+              laborum praesentium, neque provident reiciendis harum recusandae
+              excepturi aliquam repellat laboriosam facere.
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime,
+              ratione. Qui quod, dolores, veritatis itaque voluptates quaerat odit
+              laborum praesentium, neque provident reiciendis harum recusandae
+              excepturi aliquam repellat laboriosam facere.
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime,
+              ratione. Qui quod, dolores, veritatis itaque voluptates quaerat odit
+              laborum praesentium, neque provident reiciendis harum recusandae
+              excepturi aliquam repellat laboriosam facere.
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime,
+              ratione. Qui quod, dolores, veritatis itaque voluptates quaerat odit
+              laborum praesentium, neque provident reiciendis harum recusandae
+              excepturi aliquam repellat laboriosam facere.
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime,
+              ratione. Qui quod, dolores, veritatis itaque voluptates quaerat odit
+              laborum praesentium, neque provident reiciendis harum recusandae
+              excepturi aliquam repellat laboriosam facere.
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime,
+              ratione. Qui quod, dolores, veritatis itaque voluptates quaerat odit
+              laborum praesentium, neque provident reiciendis harum recusandae
+              excepturi aliquam repellat laboriosam facere.
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime,
+              ratione. Qui quod, dolores, veritatis itaque voluptates quaerat odit
+              laborum praesentium, neque provident reiciendis harum recusandae
+              excepturi aliquam repellat laboriosam facere.
+            </p>
+          </div>
+        </div>
       </div>
     </main>
   );
