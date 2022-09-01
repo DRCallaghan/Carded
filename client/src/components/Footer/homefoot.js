54e6ed883f66cbe866e5a51bbc8d280dbb36ff31
StevenBolc <steven.bolcar95@gmail.com>
2022-09-01 13:29:28 -0400

    added more detail to modals, created backButton.js

--- client/src/components/Footer/homefoot.js
+++ client/src/components/Footer/homefoot.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useLocation, useNavigate } from 'react-router-dom';
+
 
 const HomeFooter = () => {
   const styles = {
@@ -17,19 +17,11 @@ const HomeFooter = () => {
     },
   };
 
-  const location = useLocation();
-  const navigate = useNavigate();
+
   return (
     <footer style={styles.footerStyle} className="w-100 mt-auto text-dark p-4">
       <div style={styles.logoStyle} className="container text-center mb-5">
-        {location.pathname !== '/' && (
-          <button
-            className="btn btn-dark mb-3"
-            onClick={() => navigate(-1)}
-          >
-            &larr; Go Back
-          </button>
-        )}
+      
         <h4>&copy; {new Date().getFullYear()} - Tech Friends</h4>
       </div>
     </footer>
--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -9,10 +9,11 @@ import CardExample from '../components/Bootstrap/card';
 import Drop from '../components/Bootstrap/dropdown';
 import HomeFooter from '../components/Footer/homefoot';
 import SearchModal from '../components/Bootstrap/searchModal';
+import BackButton from '../components/Bootstrap/backButton';
 
 const Profile = () => {
   const { profileId } = useParams();
-
+ 
   // If there is no `profileId` in the URL as a parameter, execute the `QUERY_ME` query instead for the logged in user's information
   const { loading, data } = useQuery(
     profileId ? QUERY_SINGLE_PROFILE : QUERY_ME,
@@ -42,13 +43,19 @@ const Profile = () => {
     );
   }
 
+  const styles = {
+    navbar:{
+      alignItems: 'center'
+    }
+  }
+
   return (
     <div>
-      <nav>
+      <nav style={styles.navbar}>
         <SearchModal />
-        <Drop />
+        <BackButton/>
+        <Drop/>
       </nav>
-
       <div>
         <CardExample />
       </div>
@@ -58,7 +65,6 @@ const Profile = () => {
           isLoggedInUser={!profileId && true}
         />
       )}
-
       <div className="my-4 p-4" style={{ border: '1px dotted #1a1a1a' }}>
         <PhoneForm profileId={profile._id} />
       </div>
