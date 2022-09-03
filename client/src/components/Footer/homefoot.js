092fd2c42ba3326a9d4775dd7f14ab49739fbb01
StevenBolc <steven.bolcar95@gmail.com>
2022-09-02 21:01:17 -0400

    fixed some footer styling

--- client/src/components/Footer/homefoot.js
+++ client/src/components/Footer/homefoot.js
@@ -10,15 +10,14 @@ const HomeFooter = () => {
       display: 'flex',
       justifyContent: "space-between",
       height: '20vh',
-      fontSize: '2.87vw',
+      fontSize: 'max(2.5vh + 1.5vw)',
       color: '#14397d',
       background: 'linear-gradient(20deg, #77b5d9, #d7eaf3, #d7eaf3,#77b5d9)',
+      
     },
     logoStyle: {
       position: 'relative',
-      paddingTop: '10px',
-      paddingBottom: '10px',
-      fontSize: 'max(2.5vh + 1.5vw)',
+      fontSize: 'max(2.5vh + 1.7vw)',
       textShadow: ' 2px 2px  rgba(0,0,0,.2)',
       color: '#14397d',
     },
@@ -28,16 +27,20 @@ const HomeFooter = () => {
       marginTop: '7px',
       minWidth: '120px'
     },
+    extLinks:{
+      paddingTop: '2.5vh',
+      display: 'flex'
+    }
   };
 
   return (
     <footer style={styles.footerStyle} className="w-100 mt-auto text-dark p-4">
-      <div className='center'>
+      <div style={styles.extLinks} className='center'>
         <div><a href='https://github.com/DRCallaghan'><FaGithub /></a><a href='https://www.linkedin.com/in/dennis-russell-callaghan/'><FaLinkedin /></a>
           <p>Dennis Callaghan</p>
         </div>
       </div>
-      <div className='center'>
+      <div style={styles.extLinks} className='center'>
         <div><a href='https://github.com/pbnj1'><FaGithub /></a><a href='https://www.linkedin.com/in/pete-wang-SWE'><FaLinkedin /></a>
           <p>Peter Wang</p>
         </div>
@@ -48,12 +51,12 @@ const HomeFooter = () => {
         <h4 style={styles.logoStyle}>Carded</h4>
       </div>
 
-      <div className='center'>
-        <div><a href='https://github.com/Ckratz17'><FaGithub /></a><a href='https://www.linkedin.com/in/christopher-kratz-2a0714247/'><FaLinkedin /></a>
+      <div style={styles.extLinks} className='center'>
+        <div><a  href='https://github.com/Ckratz17'><FaGithub /></a><a href='https://www.linkedin.com/in/christopher-kratz-2a0714247/'><FaLinkedin /></a>
           <p>Chris Kratz</p>
         </div>
       </div>
-      <div className='center'>
+      <div style={styles.extLinks} className='center'>
         <div ><a href='https://github.com/StevenBolc'><FaGithub /></a><a href='www.linkedin.com/in/StevenBolc'><FaLinkedin /></a>
           <p>Steven Bolcar</p>
         </div>
