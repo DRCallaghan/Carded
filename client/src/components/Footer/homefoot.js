f450b6441503088c35ced28475f8df31646218e7
Chris Kratz <Ckratz17@gmail.com>
2022-09-03 13:59:41 -0400

    repush

--- client/src/components/Footer/homefoot.js
+++ client/src/components/Footer/homefoot.js
@@ -54,7 +54,7 @@ const HomeFooter = () => {
 
       <div style={styles.extLinks} className='center'>
         <div><a  href='https://github.com/Ckratz17'><FaGithub /></a><a href='https://www.linkedin.com/in/christopher-kratz-2a0714247/'><FaLinkedin /></a>
-          <p>Christopher Kratz</p>
+          <p>Chris Kratz</p>
         </div>
       </div>
       <div style={styles.extLinks} className='center'>
